c13dea45043270e14da3861d5f56284a4efce6e8
markuskunej <markuskunej@hotmail.com>
2020-11-20 23:30:12 -0500

    added player and team csv's for each season, and collected data from the German Bundesliga and Spanish La Liga

--- collect_from_api.py
+++ collect_from_api.py
@@ -8,7 +8,8 @@ import os
 
 ## CONSTANTS ##
 URL = "https://api.footystats.org"  # non-changing base url
-leagueName = "England Premier League"
+# examples = "England Premier League", "Germany Bundesliga", "Spain La Liga", etc.
+leagueName = "Spain La Liga"
 apiKey = '3c87c3773c4ff88f7cbdc8f44e4ced63130a5756031056c90b9025b5ffe62407'
 
 ## FUNCTIONS ##
@@ -54,33 +55,64 @@ def getMatchDetails(matchIds):
         #    count = count + 1
 
     return matchDetails
-      
 
-## MAIN CODE ##
-seasonIds = [3119, 3121, 3125, 3131, 3137, 4759]
-#seasonIds = getSeasonIds(leagueName)    #get season id's for specified league
-#print(seasonIds)
-allSeasons = pd.DataFrame()     #empty data frame to populate
+def getTeams(seasonId):
 
-for season_id in seasonIds:
-    matchIds = getMatchIds(season_id)
-    #print(matchIds)
-    matchDetails = getMatchDetails(matchIds)
-    df = pd.json_normalize(matchDetails)
-    allSeasons = allSeasons.append(df)    #append to sum dataframe
+    response = requests.get(URL + '/league-teams', params={'key': apiKey, 'season_id': seasonId})
+    teams = response.json()['data']
 
-    #produce path for file
-    outname = "{}.csv".format(season_id)
+    return teams
+
+def getPlayers(seasonId):
+    allPlayers = []
+
+    response = requests.get(URL + '/league-players', params={'key': apiKey, 'season_id': seasonId})
+    pages = response.json()['pager']['max_page']
+    
+    for page in range(1, pages+1):      #must manually loop through pages
+        response = requests.get(URL + '/league-players', params={'key': apiKey, 'season_id': seasonId, 'page': page})
+        players = response.json()['data']
+
+        allPlayers = allPlayers + players
+
+    return allPlayers
+
+def produceCSV(jsonData, fileName):
+
+    df = pd.json_normalize(jsonData)
+
     outdir = "./footy_api_csv_data/{}".format(leagueName)
     if not os.path.exists(outdir):        #check if directory exists and make it if it doesn't
         os.mkdir(outdir)
 
-    fullname = os.path.join(outdir, outname)
+    fullname = os.path.join(outdir, fileName)
+    df.to_csv(fullname, index = False)
 
-    df.to_csv(fullname, index=False)
+    return True
+
+
+## MAIN CODE ##
+
+seasonIds = getSeasonIds(leagueName)    #get season id's for specified league
+#print(seasonIds)
+#allSeasons = pd.DataFrame()     #empty data frame to populate
+
+for season_id in seasonIds:
+    #matchIds = getMatchIds(season_id)
+    #print(matchIds)
+    #matchDetails = getMatchDetails(matchIds)
+    #allSeasons = allSeasons.append(df)    #append to sum dataframe
+    teams = getTeams(season_id)
+    players = getPlayers(season_id)
+
+    # Produce CSV's #
+    #produceCSV(matchDetails, "{}.csv".format(season_id))
+    produceCSV(teams, "{}_teams.csv".format(season_id))
+    produceCSV(players, "{}_players.csv".format(season_id))
+
     print("finished season {}".format(season_id))
 
-allSeasons.to_csv(outdir + '/all_seasons.csv', index=False)
+#allSeasons.to_csv(outdir + '/all_seasons.csv', index=False)
 print("Done creating csv's for all {} seasons of the {}".format(len(seasonIds), leagueName))
 
    
